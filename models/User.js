6ada8c8e72771d2a630162d0db2f10b662fa45f9
Anurag Singh <anuragthakur102030@gmail.com>
2026-03-28 01:26:39 +0530

    fix: correct matchPassword method

--- models/User.js
+++ models/User.js
@@ -42,9 +42,8 @@ userSchema.pre('save', async function(next){
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-userSchema.methods,matchPassword = async function(enteredPassword){
+userSchema.methods.matchPassword = async function(enteredPassword){
   return await bcrypt.compare(enteredPassword, this.password);
-  next();
 };
 
 module.exports = mongoose.model('User', userSchema);
